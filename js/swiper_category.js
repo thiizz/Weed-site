a33030533646d33d38be8949d2e5bea43235c77e
thiizz <77014702+thiizz@users.noreply.github.com>
2022-07-31 13:23:28 -0300

    Add files via upload

---
+++ js/swiper_category.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 'auto',
+	spaceBetween: 6,
+	centeredSlides: true,
+	slidesPerGroup: 5,
+	simulateTouch : true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+});
